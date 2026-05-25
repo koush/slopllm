7e8272bcdab3f2915753c7081c656defee1ac51b
Koushik Dutta <koushd@gmail.com>
2026-05-25 18:05:06 +0000

    ExecutionState: support computing all logits

--- src/execution-workspace.ts
+++ src/execution-workspace.ts
@@ -35,7 +35,7 @@ export class ExecutionState {
     this.qoIndptrHost = qoIndptrHost;
   }
 
-  computeLogits(hiddenStates: Tensor, model: ChatModel, lastIdx: Tensor | undefined = this.ws.lastIdx): Tensor {
+  computeLogits(hiddenStates: Tensor, model: ChatModel, lastIdx: Tensor | null = this.ws.lastIdx): Tensor {
     const lmHead = model.tensors.get("lm_head.weight")!;
     const hs = model.cfg.hiddenSize;
     const batchSize = this.batchSize;
