cacc62b995386f3c42e2a3c2d1e7a4fda166605d
Koushik Dutta <koushd@gmail.com>
2026-06-09 21:12:28 +0000

    p2p single gpu signaling

--- Makefile
+++ Makefile
@@ -9,6 +9,7 @@ BUILD_DIR := build/Release
 SRCS_CU := csrc/glm_ops.cu csrc/glm_flash.cu csrc/glm_gemv.cu csrc/glm_gdn.cu csrc/glm_sampling.cu csrc/glm_p2p.cu csrc/glm_context_parallel.cu csrc/glm_grouped_moe.cu
 SRCS_CPP := csrc/glm_nccl.cpp csrc/glm_device.cpp
 OBJS := $(SRCS_CU:csrc/%.cu=$(BUILD_DIR)/%.o) $(SRCS_CPP:csrc/%.cpp=$(BUILD_DIR)/%.o)
+DEPS := $(OBJS:.o=.d)
 
 .PHONY: all clean test
 
@@ -21,16 +22,18 @@ $(BUILD_DIR)/$(LIB_NAME): $(OBJS) | $(BUILD_DIR)
 		-L$(CUDA_PATH)/lib64 -lcublas -lcudart -lnccl
 
 $(BUILD_DIR)/%.o: csrc/%.cu | $(BUILD_DIR)
-	$(NVCC) $(NVCC_FLAGS) -c -o $@ $< \
+	$(NVCC) $(NVCC_FLAGS) -MD -MF $(@:.o=.d) -c -o $@ $< \
 		$(FLASHINFER_INC) \
 		-I$(CUDA_PATH)/include
 
 $(BUILD_DIR)/%.o: csrc/%.cpp | $(BUILD_DIR)
-	$(NVCC) $(NVCC_FLAGS) -c -o $@ $< \
+	$(NVCC) $(NVCC_FLAGS) -MD -MF $(@:.o=.d) -c -o $@ $< \
 		-I$(CUDA_PATH)/include
 
 $(BUILD_DIR):
 	mkdir -p $(BUILD_DIR)
 
 clean:
-	rm -f $(BUILD_DIR)/$(LIB_NAME) $(OBJS)
+	rm -f $(BUILD_DIR)/$(LIB_NAME) $(OBJS) $(DEPS)
+
+-include $(DEPS)
--- csrc/glm_p2p_common.cuh
+++ csrc/glm_p2p_common.cuh
@@ -30,4 +30,58 @@ __device__ __forceinline__ void p2p_publish_and_wait(
     __threadfence_system();
 }
 
+__device__ __forceinline__ int p2p_publish_and_wait_any(
+    int tid,
+    int* const* s_peer_flags,
+    int my_rank,
+    int world_size,
+    int seq,
+    int prev_ready_mask)
+{
+    int full_mask = ((1 << world_size) - 1) ^ (1 << my_rank);
+
+    if (prev_ready_mask == full_mask) return -1;
+
+    __shared__ volatile int smem_flag_ready;
+    if (tid == 0) smem_flag_ready = 0;
+
+    if (prev_ready_mask == 0) {
+        __threadfence_system();
+        __syncthreads();
+        if (tid == 0) {
+            volatile int* mf = s_peer_flags[my_rank];
+            *mf = seq + 1;
+        }
+    }
+    __syncthreads();
+
+    while (true) {
+        if (tid < world_size && tid != my_rank
+            && !(prev_ready_mask & (1 << tid))) {
+            volatile int* pf = s_peer_flags[tid];
+            if (*pf >= seq + 1) {
+                smem_flag_ready = 1;
+                break;
+            }
+        }
+        if (smem_flag_ready) break;
+    }
+
+    __shared__ int s_ready_mask;
+    if (tid < 32) {
+        __syncwarp();
+        uint32_t my_bit = 0;
+        if (tid < world_size && tid != my_rank) {
+            volatile int* pf = s_peer_flags[tid];
+            if (*pf >= seq + 1) my_bit = (1u << tid);
+        }
+        uint32_t ballot = __ballot_sync(0xFFFFFFFFu, my_bit != 0);
+        if (tid == 0) s_ready_mask = (int)ballot;
+    }
+    __syncthreads();
+    __threadfence_system();
+
+    return s_ready_mask;
+}
+
 #endif
