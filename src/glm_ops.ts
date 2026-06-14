8c81816ad900e227300e9d9c409a67f836ffda24
Koushik Dutta <koushd@gmail.com>
2026-06-14 01:46:35 +0000

    fixup mmap load

--- src/glm_ops.ts
+++ src/glm_ops.ts
@@ -368,15 +368,15 @@ export class GlmTensor extends Tensor {
   }
 
   async mmapLoad(mmapPtr: number, offset: number, nbytes: number, strided?: StridedMmap): Promise<void> {
-    // if (strided) {
-    //   return this.memcpy2dHostToDeviceAsync(strided.dstOffset, strided.dstPitch, mmapPtr + offset + strided.srcOffset, strided.srcPitch, strided.width, strided.height);
-    // } else {
-    //   return this.mmapLoadAsync(mmapPtr, offset, nbytes);
-    // }
+    if (strided) {
+      return this.memcpy2dHostToDeviceAsync(strided.dstOffset, strided.dstPitch, mmapPtr + offset + strided.srcOffset, strided.srcPitch, strided.width, strided.height);
+    } else {
+      return this.mmapLoadAsync(mmapPtr, offset, nbytes);
+    }
   }
 
   async mmapLoadAsync(mmapPtr: number, offset: number, nbytes: number): Promise<void> {
-    // return getNativeAddon().mmapLoadAsync(this.glm.ctx, this.data, mmapPtr, offset, nbytes);
+    return getNativeAddon().mmapLoadAsync(this.glm.ctx, this.data, mmapPtr, offset, nbytes);
   }
 
   memcpy2dHostToDeviceAsync(dstOffset: number, dpitch: number, src: number, spitch: number, width: number, height: number): Promise<void> {
